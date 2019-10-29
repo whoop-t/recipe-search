3eacc9b352df1b18888a29b1d34e6d8c43f6a7c3
Tommy Mallis <tommymallis@gmail.com>
2019-10-29 16:45:11 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Form from './components/Form';
+import Recipes from './components/Recipes';
+import './App.css';
+
+const API_KEY = '98f11fb71fd58670a5d64c2cda611a22';
+
+class App extends Component {
+  state = {
+    recipes: []
+  };
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(
+      `https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}`
+    );
+
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes });
+  };
+
+  componentDidMount = () => {
+    const json = localStorage.getItem('recipes');
+    const recipes = JSON.parse(json);
+    this.setState({ recipes: recipes });
+  };
+
+  componentDidUpdate = () => {
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem('recipes', recipes);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Recipe Search</h1>
+        </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Recipe.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+const API_KEY = '98f11fb71fd58670a5d64c2cda611a22';
+
+class Recipe extends Component {
+  state = {
+    activeRecipe: []
+  };
+  componentDidMount = async () => {
+    const title = this.props.location.state.recipe;
+    const req = await fetch(
+      `https://www.food2fork.com/api/search?key=${API_KEY}&q=${title}`
+    );
+
+    const res = await req.json();
+    this.setState({ activeRecipe: res.recipes[0] });
+  };
+  render() {
+    const recipe = this.state.activeRecipe;
+    if (this.state.activeRecipe === undefined) {
+      return (
+        <div className="jumbotron jumbotron-fluid">
+          <div className="container">
+            <h1 className="display-4">No Details</h1>
+            <button className="active-recipe__button">
+              <Link to="/">Go Home</Link>
+            </button>
+          </div>
+        </div>
+      );
+    }
+    return (
+      <div className="container">
+        {this.state.activeRecipe.length !== 0 && (
+          <div className="active-recipe">
+            <img
+              className="active-recipe__img"
+              src={recipe.image_url}
+              alt={recipe.title}
+            />
+            <h3 className="active-recipe__title">{recipe.title}</h3>
+            <h4 className="active-recipe__publisher">
+              Publisher: <span>{recipe.publisher}</span>
+            </h4>
+            <p className="active-recipe__website">
+              Website:
+              <span>
+                <a href={recipe.publisher_url}>{recipe.publisher_url}</a>
+              </span>
+            </p>
+            <button className="active-recipe__button">
+              <Link to="/">Go Home</Link>
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Recipe;
